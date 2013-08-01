e88078d31bf2b563cebda93a8f126384633e04e8
Jason Noble <github+jasonn@jasonnoble.org>
2013-07-31 21:40:07 -0600

    Palindrome code as it was at 9:40

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,2 +1,19 @@
 class Palindrome
+  def self.palindrome?(string)
+    string.downcase!
+    string.gsub!(/\W/, '')
+    if string.length < 2
+      true
+    else
+      #string[0] == string[-1] && string[1] == string[-2]
+      #characters[0] == characters[-1] &&
+      #    characters[1] == characters[-2]
+      characters = string.split('')
+      first_character = characters.shift
+      last_character = characters.pop
+      rest_of_string = characters.join('')
+      first_character == last_character &&
+          palindrome?(rest_of_string)
+    end
+  end
 end
--- spec/lib/palindrome_spec.rb
+++ spec/lib/palindrome_spec.rb
@@ -1,4 +1,82 @@
 require './lib/palindrome'
 
 describe Palindrome do
+  describe "#palindrome?" do
+    it "is true for empty string" do
+      expect(Palindrome.palindrome?('')).to be_true
+    end
+
+    it "is true for single character string" do
+      expect(Palindrome.palindrome?('a')).to be_true
+      expect(Palindrome.palindrome?('A')).to be_true
+    end
+
+    it "is true for strings with only special characters" do
+      expect(Palindrome.palindrome?(%q{,.!?;:'"-})).to be_true
+    end
+
+    it "is true for strings with only white space" do
+      expect(Palindrome.palindrome?(%q{       })).to be_true
+    end
+
+    describe "for two letter strings" do
+      it "is true if the characters are the same" do
+        expect(Palindrome.palindrome?('aa')).to be_true
+        expect(Palindrome.palindrome?('AA')).to be_true
+        expect(Palindrome.palindrome?('Aa')).to be_true
+      end
+
+      it "is false if the characters are not the same" do
+        expect(Palindrome.palindrome?('ab')).to be_false
+      end
+    end
+
+    describe "for three letter strings" do
+      it "is true if the first and last character match" do
+        expect(Palindrome.palindrome?('aba')).to be_true
+      end
+
+      it "is false if the first and last character do not match" do
+        expect(Palindrome.palindrome?('abc')).to be_false
+      end
+    end
+
+    describe "for four letter strings" do
+      describe "if the first and last character match" do
+        it "is true if the inner characters match" do
+          expect(Palindrome.palindrome?('abba')).to be_true
+        end
+
+        it "is false if the inner characters do not match" do
+          expect(Palindrome.palindrome?('abca')).to be_false
+        end
+      end
+    end
+
+    describe "common palindromes" do
+      it "is true for dad" do
+        expect(Palindrome.palindrome?('dad')).to be_true
+      end
+
+      it "is true for radar" do
+        expect(Palindrome.palindrome?('radar')).to be_true
+      end
+
+      it "is true for racecar" do
+        expect(Palindrome.palindrome?('racecar')).to be_true
+      end
+
+      it "is true for deed" do
+        expect(Palindrome.palindrome?('deed')).to be_true
+      end
+
+      it "A man, a plan, a canal, Panama!" do
+        expect(
+            Palindrome.palindrome?(
+                   'A man, a plan, a canal, Panama!'
+            )
+        ).to be_true
+      end
+    end
+  end
 end
